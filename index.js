a6e4b240527c7636c2a4d53a71610c59dedf6fa4
SilantievMax <nohrin.max@yandex.ru>
2023-04-14 18:16:50 +0300

    relis tasc

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
+const hourCase = ["час", "часа", "часов"];
+const minCase = ["минута", "минуты", "минут"];
+const seconndCase = ["секунда", "секунды", "секунд"];
+
+const caseDefinition = (n, arr) => {
+  let m = Math.abs(n) % 100;
+  let n1 = m % 10;
+  if (m > 10 && m < 20) {
+    return arr[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return arr[1];
+  }
+  if (n1 == 1) {
+    return arr[0];
+  }
+  return arr[2];
+};
+
+const transformationTimeInFormat = (seconds) => {
+  const hour = Math.floor(seconds / 3600);
+  const min = Math.floor(seconds / 60) - hour * 60;
+  const seconnd = seconds - (hour * 60 + min) * 60;
+  return `${hour}:${min}:${seconnd} - ${hour} ${caseDefinition(hour, hourCase)}, ${min} ${caseDefinition(min, minCase)}, ${seconnd} ${caseDefinition(
+    seconnd,
+    seconndCase
+  )}`;
+};
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    const idSetInterval = setInterval(() => {
+      timerEl.textContent = transformationTimeInFormat(seconds);
+      seconds--;
+      if (seconds < 0) {
+        timerEl.textContent = "Время кончилось!";
+        clearInterval(idSetInterval);
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener("input", () => {
+  inputEl.value = inputEl.value.replace(/[^0-9\.]/g, "");
   // Очистите input так, чтобы в значении
   // оставались только числа
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
